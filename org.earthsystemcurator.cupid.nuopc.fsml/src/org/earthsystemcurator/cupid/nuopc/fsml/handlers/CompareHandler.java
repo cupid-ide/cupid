dea579a5bde432d01b96116592f75aa1f4246dc8
Rocky Dunlap <rocky@cc.gatech.edu>
2013-07-19 14:52:25 +0200

    CompareHandler

---
+++ org.earthsystemcurator.cupid.nuopc.fsml/src/org/earthsystemcurator/cupid/nuopc/fsml/handlers/CompareHandler.java
@@ -0,0 +1,144 @@
+package org.earthsystemcurator.cupid.nuopc.fsml.handlers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.InvocationTargetException;
+
+import org.earthsystemcurator.cupid.nuopc.fsml.mapping.ModelToModuleMapping;
+import org.earthsystemcurator.cupid.nuopc.fsml.nuopc.Model;
+import org.earthsystemcurator.cupid.nuopc.fsml.nuopc.NUOPCPackage;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.Diff;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.Match;
+import org.eclipse.emf.compare.match.DefaultComparisonFactory;
+import org.eclipse.emf.compare.match.DefaultEqualityHelperFactory;
+import org.eclipse.emf.compare.match.DefaultMatchEngine;
+import org.eclipse.emf.compare.match.IComparisonFactory;
+import org.eclipse.emf.compare.match.IMatchEngine;
+import org.eclipse.emf.compare.match.eobject.IEObjectMatcher;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryImpl;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryRegistryImpl;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.compare.utils.UseIdentifiers;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.photran.core.IFortranAST;
+import org.eclipse.photran.internal.core.parser.ASTModuleNode;
+import org.eclipse.photran.internal.core.vpg.PhotranVPG;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ltk.core.refactoring.TextFileChange;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+@SuppressWarnings("restriction")
+public class CompareHandler extends AbstractHandler {
+
+	public static String reverseFile = "C:\\Users\\Rocky\\Documents\\eclipse\\runtime-nuopc\\nuopc\\Reverse.nuopc";
+
+	/**
+	 * The constructor.
+	 */
+	public CompareHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+
+		@SuppressWarnings("unused")
+		NUOPCPackage pack = NUOPCPackage.eINSTANCE;
+
+		Comparison comp = compare();
+		for (Match m : comp.getMatches()) {
+			System.out.println("Match: " + m.toString());			
+		}
+		for (Diff d : comp.getDifferences()) {
+			System.out.println("Diff: " + d.toString());			
+		}
+
+		return null;
+	}
+
+
+	public Comparison compare() {
+		// Load the two input models
+		ResourceSet resourceSet1 = new ResourceSetImpl();
+		ResourceSet resourceSet2 = new ResourceSetImpl();
+		String xmi1 = ReverseHandler.reverseFile;
+		String xmi2 = ForwardHandler.assertedFile;
+		load(xmi1, resourceSet1);
+		load(xmi2, resourceSet2);
+
+		// Configure EMF Compare
+		IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
+		IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory());
+		IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory);
+		matchEngineFactory.setRanking(20);
+		IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl();
+		matchEngineRegistry.add(matchEngineFactory);
+		EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build();
+
+		// Compare the two models
+		IComparisonScope scope = EMFCompare.createDefaultScope(resourceSet1, resourceSet2);
+		return comparator.compare(scope);
+	}
+
+	private void load(String absolutePath, ResourceSet resourceSet) {
+		URI uri = URI.createFileURI(absolutePath);
+
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("nuopc", new XMIResourceFactoryImpl());
+
+		// Resource will be loaded within the resource set
+		resourceSet.getResource(uri, true);
+	}
+
+
+
+	IResource extractSelection(ISelection sel) {
+
+		System.out.println("sel = " + sel);
+
+		if (!(sel instanceof IStructuredSelection))
+			return null;
+		IStructuredSelection ss = (IStructuredSelection) sel;
+		Object element = ss.getFirstElement();
+		if (element instanceof IResource)
+			return (IResource) element;
+		if (!(element instanceof IAdaptable))
+			return null;
+		IAdaptable adaptable = (IAdaptable)element;
+		Object adapter = adaptable.getAdapter(IResource.class);
+		return (IResource) adapter;
+	}
+}
