f7c53147c3c0d61e89f587d526f881f3a1874750
rocky <rocky@cc.gatech.edu>
2014-06-04 11:15:44 -0400

    Removed binary from source control

--- ShallowWater/src/animate.m
+++ ShallowWater/src/animate.m
@@ -6,7 +6,7 @@
 % animation.
 %
 % mencoder command used on Ubuntu 12.04:
-% %mencoder mf://output/*.png -mf w=640:h=480:fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o swout.avi
+% mencoder mf://*.png -mf w=640:h=480:fps=25:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o swout.avi
 %
 % Author: Rocky Dunlap   <rocky.dunlap@noaa.gov>
 
