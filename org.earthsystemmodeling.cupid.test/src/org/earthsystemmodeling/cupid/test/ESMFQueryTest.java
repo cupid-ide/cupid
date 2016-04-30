7e03a2ec05dc73c7e0e27e368f169f29b0731f64
Rocky Dunlap <rocky.dunlap@noaa.gov>
2016-04-30 00:16:21 -0600

    Fixing broken test

--- org.earthsystemmodeling.cupid.test/src/org/earthsystemmodeling/cupid/test/ESMFQueryTest.java
+++ org.earthsystemmodeling.cupid.test/src/org/earthsystemmodeling/cupid/test/ESMFQueryTest.java
@@ -29,7 +29,7 @@ public class ESMFQueryTest {
 	
 	@BeforeClass
 	public static void setUp() throws CoreException, IOException, InterruptedException {
-		PROJECT_NUOPC_PROTOTYPES = TestHelpers.createFortranProjectFromFolder("target/ESMF_7_0_0_beta_snapshot_59", "ESMF_7_0_0_beta_snapshot_59");
+		PROJECT_NUOPC_PROTOTYPES = TestHelpers.createFortranProjectFromFolder("target/ESMF_7_0_0", "ESMF_7_0_0");
 	}
 	
 	@AfterClass
