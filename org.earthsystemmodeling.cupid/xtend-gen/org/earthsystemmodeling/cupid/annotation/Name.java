09afeb8cfd0b08187019fe828d822318e23274be
Rocky Dunlap <rsdunlapiv@gmail.com>
2016-05-03 16:24:12 -0600

    Removing xtend files

--- org.earthsystemmodeling.cupid/xtend-gen/org/earthsystemmodeling/cupid/annotation/Name.java
+++
@@ -1,8 +0,0 @@
-package org.earthsystemmodeling.cupid.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Name {
-}
