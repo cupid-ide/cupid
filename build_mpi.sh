2fcb922ad59375cd7ad5e7caa8f1303b21b1b92e
Rocky Dunlap <rsdunlapiv@gmail.com>
2016-10-01 15:22:18 -0600

    Working on osx travis build

---
+++ build_mpi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $HOME
+if [ ! -d "$HOME/openmpi-1.8.8-install"  ]; then 
+    curl "https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz" -o "openmpi-1.8.8.tar.gz"
+    tar zxvf openmpi-1.8.8.tar.gz
+    cd openmpi-1.8.8
+    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-1.8.8-install
+    make -j4 all
+    make install
+fi
+export PATH=$HOME/openmpi-1.8.8-install:$PATH
+
