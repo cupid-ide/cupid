cb6b3e07db7acdeed9b2c963625dd981630663de
Rocky Dunlap <rsdunlapiv@gmail.com>
2016-10-01 21:56:05 -0600

    more work on scripts

--- build_mpi.sh
+++ build_mpi.sh
@@ -4,7 +4,7 @@ cd $HOME
 if [ ! -d "$HOME/openmpi-1.8.8-install/bin"  ]; then 
     curl "https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz" -o "openmpi-1.8.8.tar.gz"
     which tar
-    tar zxvf openmpi-1.8.8.tar.gz 
+    tar zxf openmpi-1.8.8.tar.gz 
     cd openmpi-1.8.8
     ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-1.8.8-install > configure.out.txt
     make -j4 all > make.out.txt
