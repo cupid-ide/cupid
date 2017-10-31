74d2c42aa2432bcc2d709c168cfd50278a4be625
Rocky Dunlap <rsdunlapiv@gmail.com>
2017-10-30 21:00:40 -0600

    Updates to fix TravisCI

--- build_mpi.sh
+++ build_mpi.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 cd $HOME
-if [ ! -d "$HOME/openmpi-1.8.8-install/bin"  ]; then 
-    curl "https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz" -o "openmpi-1.8.8.tar.gz"
+#if [ ! -d "$HOME/openmpi-1.8.8-install/bin"  ]; then 
+#    curl "https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz" -o "openmpi-1.8.8.tar.gz"
+#    which tar
+#    tar zxf openmpi-1.8.8.tar.gz 
+#    cd openmpi-1.8.8
+#    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-1.8.8-install > configure.out.txt
+#    make -j4 all > make.out.txt
+#    make install
+#fi
+if [ ! -d "$HOME/openmpi-2.1.2-install/bin"  ]; then 
+    curl "https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.2.tar.gz" -o "openmpi-2.1.2.tar.gz"
     which tar
-    tar zxf openmpi-1.8.8.tar.gz 
-    cd openmpi-1.8.8
-    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-1.8.8-install > configure.out.txt
+    tar zxf openmpi-2.1.2.tar.gz 
+    cd openmpi-2.1.2
+    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-2.1.2-install > configure.out.txt
     make -j4 all > make.out.txt
     make install
 fi
-#export PATH=$HOME/openmpi-1.8.8-install/bin:$PATH
+
 
