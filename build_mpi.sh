9de5e4b16c944277558bd4873cf7006641bff350
Rocky Dunlap <rsdunlapiv@gmail.com>
2017-11-27 13:21:35 -0700

    Fix travis build by changing openmpi version

--- build_mpi.sh
+++ build_mpi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $HOME
+### 1.8.8
 #if [ ! -d "$HOME/openmpi-1.8.8-install/bin"  ]; then 
 #    curl "https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz" -o "openmpi-1.8.8.tar.gz"
 #    which tar
@@ -10,14 +11,27 @@ cd $HOME
 #    make -j4 all > make.out.txt
 #    make install
 #fi
-if [ ! -d "$HOME/openmpi-2.1.2-install/bin"  ]; then 
-    curl "https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.2.tar.gz" -o "openmpi-2.1.2.tar.gz"
+
+### 1.10.7
+if [ ! -d "$HOME/openmpi-1.10.7-install/bin"  ]; then 
+    curl "https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.7.tar.gz" -o "openmpi-1.10.7.tar.gz"
     which tar
-    tar zxf openmpi-2.1.2.tar.gz 
-    cd openmpi-2.1.2
-    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-2.1.2-install > configure.out.txt
+    tar zxf openmpi-1.10.7.tar.gz 
+    cd openmpi-1.10.7
+    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-1.10.7-install > configure.out.txt
     make -j4 all > make.out.txt
     make install
 fi
+
+### 2.1.2
+#if [ ! -d "$HOME/openmpi-2.1.2-install/bin"  ]; then 
+#    curl "https://www.open-mpi.org/software/ompi/v2.1/downloads/openmpi-2.1.2.tar.gz" -o "openmpi-2.1.2.tar.gz"
+#    which tar
+#    tar zxf openmpi-2.1.2.tar.gz 
+#    cd openmpi-2.1.2
+#    ./configure CC=gcc CXX=g++ F77=gfortran FC=gfortran --prefix=$HOME/openmpi-2.1.2-install > configure.out.txt
+#    make -j4 all > make.out.txt
+#    make install
+#fi
 
 
